7d6ace6d795ed246e6fb889a333f01e7d95f2d0e
shuamp <castel.davila@gmail.com>
2022-08-10 10:57:04 +0100

    functionality added

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -20,11 +20,14 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public MainWindow()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             InitializeComponent();
 
             SetUpGame();
+            
         }
 
         private void SetUpGame()
@@ -39,6 +42,7 @@ namespace MatchGame
             "👻","👻",
             "🐙","🐙"
             };
+
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
@@ -47,6 +51,37 @@ namespace MatchGame
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
             }
+        }
+
+        TextBlock lastTextBlockClikcked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender,MouseButtonEventArgs e)
+        {
+            TextBlock? textBlock = sender as TextBlock;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            if (findingMatch == false)
+            {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                textBlock.Visibility = Visibility.Hidden;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                lastTextBlockClikcked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClikcked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClikcked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
         }
     }
 }
